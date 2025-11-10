de059aadb7943e633030e17574098604c1267953
Alanda <alandacherestal@gmail.com>
2025-11-10 14:02:26 -0500

    added code to index.js

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
+function createLoginTracker (userInfo){
+  let attemptCount = 0;
 
+  const loginAttempts = (passwordAtteempt) => {
+    attemptCount++;
+    if(attemptCount > 3){
+      return 'Account locked due to too many failed login attempts';
+    }
+    else if(passwordAtteempt !== userInfo.password){
+      return `Attempt ${attemptCount}: Login failed`;
+    }
+    else{return 'Login successful'};
+
+  }
+
+
+  return loginAttempts;
+}
 
 
 module.exports = {
